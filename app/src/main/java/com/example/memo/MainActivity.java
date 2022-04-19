d1dd7f706f7274396bad0034e57018f852585244
Holiday <chs11233@gmail.com>
2022-04-19 17:40:19 +0900

    first commit

---
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.memo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mloginemail,mloginpassword;
+    private RelativeLayout mlogin, mgotosignup;
+    private TextView mgotoforgotpassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    ProgressBar mprogressbarofmainactivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        mloginemail = findViewById(R.id.loginemail);
+        mloginpassword = findViewById(R.id.loginpassword);
+        mlogin = findViewById(R.id.login);
+        mgotosignup = findViewById(R.id.gotosignup);
+        mgotoforgotpassword = findViewById(R.id.gotoforgotpassword);
+        mprogressbarofmainactivity = findViewById(R.id.progressbarofmainactivity);
+
+        firebaseAuth = firebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null)
+        {
+            finish();
+            startActivity(new Intent(MainActivity.this,notesactivity.class));
+        }
+
+
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,signup.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,forgotpassword.class));
+            }
+        });
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = mloginemail.getText().toString().trim();
+                String password = mloginpassword.getText().toString().trim();
+
+                if(mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"모든 필드가 필요합니다.",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // login the user
+
+                    mprogressbarofmainactivity.setVisibility(View.VISIBLE);
+
+
+                    firebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                checkmailverfication();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"계정이 존재하지 않습니다.",Toast.LENGTH_SHORT).show();
+                                mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+                            }
+
+
+                        }
+                    });
+
+
+
+                }
+            }
+        });
+
+    }
+
+    private void checkmailverfication()
+    {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if(firebaseUser.isEmailVerified()==true)
+        {
+            Toast.makeText(getApplicationContext(),"로그인",Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(MainActivity.this,notesactivity.class));
+        }
+        else
+        {
+            mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"먼저 메일 확인",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
+}
